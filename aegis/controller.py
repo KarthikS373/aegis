ae7263a8885728329ea44f325693a7696af85d4c
karthiks373 <lit2021012@iiitl.ac.in>
2024-02-02 02:12:07 +0530

    helpers

--- aegis/controller.py
+++ aegis/controller.py
@@ -9,93 +9,8 @@ from typing import Optional
 from args import CompileArguments, ScanArguments
 from cli import Effects
 from config import BANNER
+from helpers import Helper
 from model import predict, reverse_engineer_one_hot_encoding
-
-
-def check_solcx() -> bool:
-    """ Check if solc is installed """
-    installed_versions = solcx.get_installed_solc_versions()
-    if len(installed_versions) == 0:
-        return False
-    return True
-
-
-def install_solcx(version=None) -> bool:
-    """ Install solc """
-    try:
-        if version:
-            # Install solc version
-            solcx.install_solc(version)
-            return True
-
-        # Find latest solcx version
-        latest_solcx_version = solcx.get_installable_solc_versions()[0]
-        solcx.install_solc(latest_solcx_version)
-        return True
-
-    except:
-        return False
-
-
-def get_latest_installed_solcx_version() -> Optional[str]:
-    """ Get the latest installed solcx version """
-    try:
-        latest_solcx_version = solcx.get_installed_solc_versions()[0]
-        if latest_solcx_version:
-            return latest_solcx_version
-
-        raise Exception("Could not get latest solcx version")
-
-    except:
-        return None
-
-
-def compile_solidity(contract):
-    latest_solcx_version = get_latest_installed_solcx_version()
-
-    if not latest_solcx_version:
-        print("Could not get latest solcx version")
-        return None
-
-    try:
-        compiled_file = compile_source(
-            contract,
-            output_values=["abi", "bin-runtime"],
-            solc_version=latest_solcx_version
-        )
-
-        for key, value in compiled_file.items():
-            name_of_contract = key
-            abi = value['abi']
-            bytecode = value['bin-runtime']
-
-            return {
-                "name": name_of_contract,
-                "abi": abi,
-                "bytecode": bytecode
-            }
-
-    except Exception as e:
-        return None
-
-
-def check_solidity_file(path: str) -> bool:
-    if not path.endswith(".sol"):
-        return False
-
-    return True
-
-
-def read_solidity_file(path: str):
-    try:
-        with open(path, "r") as f:
-            contract_file = f.read()
-
-        return contract_file
-
-    except Exception as e:
-        print("Error reading the file")
-        sys.exit(1)
 
 
 class Controller:
@@ -109,16 +24,19 @@ class Controller:
         # Initialize the cli typing effects
         self.effects = Effects()
 
+        # Setup the helper
+        self.helper = Helper()
+
         try:
             # Check if solc is installed
-            is_solcx_installed = check_solcx()
+            is_solcx_installed = self.helper.check_solcx()
             if not is_solcx_installed:
                 print(BANNER)
                 self.effects.skip_line(2)
                 self.effects.write("Initializing Aegis CLI.....")
 
                 # TODO: Suppress warnings
-                successfully_installed = install_solcx()
+                successfully_installed = self.helper.install_solcx()
                 if successfully_installed:
                     self.effects.skip_line(1)
                     self.effects.write("Aegis CLI is ready to use")
@@ -177,7 +95,7 @@ class Controller:
         self.effects.write("Description: Solc is the solidity compiler")
 
         installed_versions = solcx.get_installed_solc_versions()
-        latest_version = get_latest_installed_solcx_version()
+        latest_version = self.helper.get_latest_installed_solcx_version()
         installed_path = solcx.get_solcx_install_folder()
 
         if len(installed_versions) == 0:
@@ -197,40 +115,40 @@ class Controller:
 
     def compile(self, args: CompileArguments):
         """ Compile the solidity code """
-        is_solidity = check_solidity_file(args.path)
+        is_solidity = self.helper.check_solidity_file(args.path)
         if not is_solidity:
             print("Please provide a solidity file")
             return
 
-        contract_file = read_solidity_file(args.path)
+        contract_file = self.helper.read_solidity_file(args.path)
 
         self.effects.write("Compiling solidity code...")
 
         try:
-            compiled_contract = compile_solidity(
+            compiled_contract = self.helper.compile_solidity(
                 contract_file,
             )
 
             if not compiled_contract:
+                print("No compiled contract found")
                 raise Exception("Could not compile the solidity code")
-
-            # TODO: Save the compiled contract to a file
 
         except Exception as e:
             print("Error compiling the solidity code")
+            print(e)
             return
 
     def scan(self, args: ScanArguments):
         """ Scan a file or directory for vulnerabilities """
-        is_solidity = check_solidity_file(args.path)
+        is_solidity = self.helper.check_solidity_file(args.path)
         if not is_solidity:
             print("Please provide a solidity file")
             return
 
-        contract_file = read_solidity_file(args.path)
+        contract_file = self.helper.read_solidity_file(args.path)
 
         try:
-            compiled_contract = compile_solidity(
+            compiled_contract = self.helper.compile_solidity(
                 contract_file,
             )
 
@@ -248,18 +166,52 @@ class Controller:
                 5: 'unchecked-calls'
             }
 
-            bytecode = compiled_contract["bytecode"]
+            for entity in compiled_contract:
+                bytecode = entity["bytecode"]
 
-            prediction = predict(bytecode)
-            all_preds_np = np.array(prediction)
-            original_labels = reverse_engineer_one_hot_encoding(all_preds_np)
-            # print("Original Labels:", original_labels)
-            mapped_labels = [[labels[label] for label in sublist]
-                             for sublist in original_labels]
-            # print("Mapped Labels:")
-            for sublist in mapped_labels:
-                print(sublist)
+                print
+
+                if not bytecode:
+                    continue
+
+                prediction = predict(bytecode)
+                all_preds_np = np.array(prediction)
+                original_labels = reverse_engineer_one_hot_encoding(
+                    all_preds_np)
+
+                mapped_labels = [[labels[label] for label in sublist]
+                                 for sublist in original_labels]
+
+                for sublist in mapped_labels:
+                    def print_detailed_report(sublist):
+                        self.effects.write("Scan complete...")
+                        self.effects.skip_line(1)
+                        self.effects.write("Scan report")
+                        self.effects.write("-----------")
+
+                        self.effects.write("Vulnerabilities:")
+                        for label in sublist:
+                            self.effects.write(f"{label}")
+                            self.effects.write(f"--------------")
+
+                            ed = self.helper.error_descriptions[label]
+                            self.effects.write(
+                                f"[desc]: {ed['desc']}"
+                            )
+                            self.effects.write(
+                                f"[search]: {ed['search']}"
+                            )
+                            self.effects.skip_line(1)
+
+                    def print_scan_result(sublist):
+                        if 'safe' in sublist:
+                            print("Smart contract scan complete - code looks safe")
+                        else:
+                            print_detailed_report(sublist)
+
+                    print_scan_result(sublist)
 
         except Exception as e:
             print("Error scanning!!! Please try again...")
+            print(e)
             return
---
+++ aegis/helpers.py
@@ -0,0 +1,129 @@
+import solcx
+
+from solcx import compile_standard, compile_source
+from typing import Optional
+
+from args import CompileArguments, ScanArguments
+from cli import Effects
+from config import BANNER
+from model import predict, reverse_engineer_one_hot_encoding
+
+
+class Helper:
+    def __init__(self):
+        error_descriptions = {
+            'access-control': {
+                'desc': "Access control vulnerabilities occur when smart contracts do not properly control access to sensitive functions or data.",
+                'methods': "To mitigate access control vulnerabilities, ensure that sensitive functions or data can only be accessed by authorized users or contracts. Implement proper permission checks and access control mechanisms.",
+                'search': "https://ethereum.stackexchange.com/questions/153133/question-about-access-control-vulnerability"
+            },
+            'arithmetic': {
+                'desc': "Arithmetic vulnerabilities arise due to errors in mathematical calculations within smart contracts.",
+                'methods': "To address arithmetic vulnerabilities, perform thorough testing and validation of mathematical operations. Implement safe arithmetic operations and consider using tested libraries for complex calculations.",
+                'search': "https://consensys.github.io/smart-contract-best-practices/attacks/insecure-arithmetic"
+            },
+            'other': {
+                'desc': "Other vulnerabilities refer to any issues in smart contracts that do not fit into specific categories such as access control or arithmetic.",
+                'methods': "To address other vulnerabilities, conduct comprehensive code reviews and testing. Follow best practices for smart contract development and security.",
+                'search': "https://www.cvedetails.com/vulnerability-list/vendor_id-17524/Ethereum.html"
+            },
+            'reentrancy': {
+                'desc': "Reentrancy vulnerabilities occur when smart contracts are vulnerable to reentrant attacks, allowing an attacker to repeatedly call a function before previous calls are completed.",
+                'methods': "To mitigate reentrancy vulnerabilities, implement proper locking mechanisms and ensure that critical state changes are completed before external calls are made. Use the withdrawal pattern for handling external transfers of funds.",
+                'search': "https://stackoverflow.com/questions/tagged/reentrancy"
+            },
+            'safe': {
+                'desc': "The absence of known vulnerabilities indicates that the smart contract code appears to be safe from common security threats.",
+                'methods': "Continue to follow best practices for smart contract development and conduct regular security audits to maintain the code's safety.",
+                'search': ""
+            },
+            'unchecked-calls': {
+                'desc': "Unchecked calls vulnerabilities occur when smart contracts make external calls without properly validating or handling the return values.",
+                'methods': "To address unchecked calls vulnerabilities, implement proper error handling and validation mechanisms for external calls. Use safe wrappers or libraries for interacting with external contracts.",
+                'search': "https://www.bookstack.cn/read/ethereumbook-en/spilt.11.c2a6b48ca6e1e33c.md"
+            }
+        }
+
+    def check_solcx(self) -> bool:
+        """ Check if solc is installed """
+        installed_versions = solcx.get_installed_solc_versions()
+        if len(installed_versions) == 0:
+            return False
+        return True
+
+    def install_solcx(self, version=None) -> bool:
+        """ Install solc """
+        try:
+            if version:
+                # Install solc version
+                solcx.install_solc(version)
+                return True
+
+            # Find latest solcx version
+            latest_solcx_version = solcx.get_installable_solc_versions()[0]
+            solcx.install_solc(latest_solcx_version)
+            return True
+
+        except:
+            return False
+
+    def get_latest_installed_solcx_version(self) -> Optional[str]:
+        """ Get the latest installed solcx version """
+        try:
+            latest_solcx_version = solcx.get_installed_solc_versions()[0]
+            if latest_solcx_version:
+                return latest_solcx_version
+
+            raise Exception("Could not get latest solcx version")
+
+        except:
+            return None
+
+    def compile_solidity(self, contract):
+        latest_solcx_version = self.get_latest_installed_solcx_version()
+
+        if not latest_solcx_version:
+            print("Could not get latest solcx version")
+            return None
+
+        try:
+            compiled_file = compile_source(
+                contract,
+                output_values=["abi", "bin-runtime"],
+                solc_version=latest_solcx_version
+            )
+
+            contracts = []
+            for key, value in compiled_file.items():
+                name_of_contract = key
+                abi = value['abi']
+                bytecode = value['bin-runtime']
+
+                contracts.append({
+                    "name": name_of_contract,
+                    "abi": abi,
+                    "bytecode": bytecode
+                })
+
+            return contracts
+
+        except Exception as e:
+            print(e)
+            return None
+
+    def check_solidity_file(self, path: str) -> bool:
+        if not path.endswith(".sol"):
+            return False
+
+        return True
+
+    def read_solidity_file(self, path: str):
+        try:
+            with open(path, "r") as f:
+                contract_file = f.read()
+
+            return contract_file
+
+        except Exception as e:
+            print("Error reading the file")
+            sys.exit(1)
